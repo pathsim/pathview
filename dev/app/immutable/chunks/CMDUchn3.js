a8762e4f82864c8e0af8bd66cab4561586db60a8
milanofthe <milanofthe@users.noreply.github.com>
2026-01-18 19:09:07 +0000

    deploy: 92225219c41b5019d8985288e4d5bf606ed73f24

---
+++ dev/app/immutable/chunks/CMDUchn3.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1idcdo5?.base??"/dev",e=globalThis.__sveltekit_1idcdo5?.assets??s??"";export{e as a,s as b};
---
+++ dev/app/immutable/entry/start.CnNMeB--.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/DU4BlRmT.js";export{o as load_css,r as start};
