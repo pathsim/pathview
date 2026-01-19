b5d0b3b87c9efd84a18eb5e43fe607663fe44b93
milanofthe <milanofthe@users.noreply.github.com>
2026-01-19 09:01:06 +0000

    deploy: 99f4f63a5f86664972c00f905fe7262f4080e3c8

---
+++ dev/app/immutable/chunks/DqAzxoeZ.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_14udntj?.base??"/dev",t=globalThis.__sveltekit_14udntj?.assets??s??"";export{t as a,s as b};
---
+++ dev/app/immutable/entry/start.O2EOR08P.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/A2vM1U3s.js";export{o as load_css,r as start};
