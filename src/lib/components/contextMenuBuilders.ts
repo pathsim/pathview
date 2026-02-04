3e7cbefd0c3aecaaa12a12e3baaf700268fad470
Milan <milan@MacBook-Air-von-Milan.local>
2026-02-04 16:51:19 +0100

    Add tag icon for port label menu items

--- src/lib/components/contextMenuBuilders.ts
+++ src/lib/components/contextMenuBuilders.ts
@@ -207,14 +207,14 @@ function buildNodeMenu(nodeId: string): MenuItemType[] {
 		DIVIDER,
 		{
 			label: showInputLabels ? 'Hide Input Labels' : 'Show Input Labels',
-			icon: 'type',
+			icon: 'tag',
 			action: () => historyStore.mutate(() =>
 				graphStore.updateNodeParams(nodeId, { _showInputLabels: !showInputLabels })
 			)
 		},
 		{
 			label: showOutputLabels ? 'Hide Output Labels' : 'Show Output Labels',
-			icon: 'type',
+			icon: 'tag',
 			action: () => historyStore.mutate(() =>
 				graphStore.updateNodeParams(nodeId, { _showOutputLabels: !showOutputLabels })
 			)
